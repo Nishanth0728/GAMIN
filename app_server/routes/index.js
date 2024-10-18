d7e235735a32da9869d5a30b4199eee74ffc2098
Nishanth0728 <22eg107c07@anurag.edu.in>
2024-10-18 12:04:06 +0530

    final commit

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -6,7 +6,7 @@ const gamesOthers = require('../controllers/others');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'GAMIN' });
 });
 
 router.get('/games', gamesController.getGames);
